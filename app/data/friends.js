a39feb29c0be499d0911628f90fd59f040259141
Valerie Fragier <valfragier@gmail.com>
2018-10-29 10:28:08 -0400

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Valerie Fragier',
+      profilePic: 'https://pbs.twimg.com/profile_images/641658747983630337/qzSMVaJf_400x400.jpg',
+      scores: [
+        "5",
+        "1",
+        "5",
+        "3",
+        "5",
+        "1",
+        "3",
+        "4",
+        "3",
+        "5"
+      ]
+    },
+    {
+		name: 'Harry Potter',
+		photo: 'https://vignette.wikia.nocookie.net/harrypotter/images/c/c1/Harry%2Bpotter-Harry_Potter_HP4_01.jpg/revision/latest?cb=20120207172914',
+		scores: [
+			'1',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'3',
+			'1',
+			'2'
+		]
+    },
+    {
+		name: 'Michelle Obama',
+		photo: 'https://media2.fdncms.com/metrotimes/imager/u/original/15562381/screen_shot_2018-09-13_at_7.55.25_am.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'3',
+			'5',
+			'1',
+			'4',
+			'3',
+			'5',
+			'3'
+		]
+	}
+  ];
+  
+  module.exports = friendList;
